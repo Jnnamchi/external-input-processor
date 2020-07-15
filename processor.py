26f94fe238440531999635480fa43a73d192febb
John Nnamchi <john.nnamchi@mail.mcgill>
2020-07-15 13:53:30 -0400

    Obtained user name from terminal input

---
+++ processor.py
@@ -0,0 +1,9 @@
+print("\nWelcome to the external input processor, I will process and analyze data from anywhere!\n")
+
+# Get Username
+
+# Print message prompting user to input their name
+username = input("To begin, please enter your username:\n")
+
+# Greet the user
+print("Hello, " + username)
